61c86f7db93c83ed9054d5a9b9addbf898770cf6
Lythium4848 <max@lythium.dev>
2024-05-06 21:55:48 +0100

    feat: Route to get trains from a CRS to another

--- src/main/kotlin/Routes/Departure.kt
+++ src/main/kotlin/Routes/Departure.kt
@@ -16,7 +16,7 @@ fun Route.departure() {
 				)
 
 				val filterCRS = call.parameters["filterCRS"] ?: ""
-				val filterType = call.parameters["filterType"] ?: ""
+				val filterType = call.parameters["filterType"] ?: "to"
 				val timeOffset = call.parameters["timeOffset"]?.toIntOrNull() ?: 0
 				val timeWindow = call.parameters["timeWindow"]?.toIntOrNull() ?: 120
 
@@ -31,6 +31,32 @@ fun Route.departure() {
 
 				val data = ldbws.getDepartureBoard(10, crs, filterCRS, filterType, timeOffset, timeWindow)
 				call.respond(data)
+			}
+
+			route("/to") {
+				get("{crs2}") {
+					val fromCRS = call.parameters["crs"] ?: return@get call.respondText(
+						"From CRS not supplied. Please specify a From CRS",
+						status = HttpStatusCode.BadRequest
+					)
+
+					val toCRS = call.parameters["crs2"] ?: return@get call.respondText(
+						"To CRS not supplied. Please specify a To CRS",
+						status = HttpStatusCode.BadRequest
+					)
+
+					val timeOffset = call.parameters["timeOffset"]?.toIntOrNull() ?: 0
+					val timeWindow = call.parameters["timeWindow"]?.toIntOrNull() ?: 120
+
+					fromCRS.length != 3 && return@get call.respondText("From CRS must be 3 characters long.", status = HttpStatusCode.BadRequest)
+					toCRS.length != 3 && return@get call.respondText("To CRS must be 3 characters long.", status = HttpStatusCode.BadRequest)
+
+					Stations.getStation(crs = fromCRS) == null && return@get call.respondText("Invalid From CRS code.", status = HttpStatusCode.BadRequest)
+					Stations.getStation(crs = toCRS) == null && return@get call.respondText("Invalid To CRS code.", status = HttpStatusCode.BadRequest)
+
+					val data = ldbws.getDepartureBoard(10, fromCRS, toCRS, "to", timeOffset, timeWindow)
+					call.respond(data)
+				}
 			}
 		}
 	}
